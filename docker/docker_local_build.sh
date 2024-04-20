a71a02d5f363f40c421b91540642d7894848d227
lacie-life <00sao00ios00@gmail.com>
2024-04-20 15:25:40 +0900

    [Update] docker images local build for setup_env

--- docker/docker_local_build.sh
+++ docker/docker_local_build.sh
@@ -6,7 +6,7 @@ docker build --build-arg QT_VERSION_MAJOR=5 \
              --build-arg QT_VERSION_MINOR=15 \
              --build-arg QT_VERSION_PATCH=2 \
              --build-arg CORE_COUNT=12 \
-             --target=qt -t simple_viz:5.15.2 \
+             --target=qt -t lifezero/simple_viz:5.15.2 \
              .
 
 
